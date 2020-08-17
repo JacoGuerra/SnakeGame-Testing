f454c237edd005c23445bfd112bec8e3defd12ea
JacoGuerra <Jacobofguerra@gmail.com>
2020-08-18 00:07:40 +0300

    Snake Games update

--- Snake_Game.py
+++ Snake_Game.py
@@ -40,6 +40,66 @@ class snake(object):
         pass
 
 
+def drawGrid(w, rows, surface):
+    sizeBtwn = w//rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
+
+
+def redrawWindow(surface):
+    global rows, width
+    surface.fill((0,0,0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+
+
+def randomSnack(rows, items):
+    pass
+
+def message_box(subkect, contect):
+    pass
+
+
+def main():
+    global width, rows
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    s= snake((255, 0, 0), (10, 10))
+    flag= True
+
+    clock=pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50) #delay of 50 miliseconds
+        clock.tick(10) #running not more than 10 frames per second
+        redrawWindow(win)
+#rows =
+#w =
+#h =
+#
+#cube.rows =rows
+#cube.w=w
+
+main()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
